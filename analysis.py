505a95e06206fae6628ea86fe1f5a2ed29b592a3
snowStorrm <ryanjz101@gmail.com>
2025-11-26 20:13:39 -0500

    Data export formatting changes

--- analysis.py
+++ analysis.py
@@ -2,10 +2,14 @@ import pandas, openpyxl
 
 # Simply exports two arrays to an excel sheet for data analysis
 # - Will append to an existing excel file if it exists
-def exportResults(axisX, axisY, name=f"Sheet{len(openpyxl.load_workbook('./out.xlsx').sheetnames)}"):
+def exportResults(axisX, axisY, name=f"Sheet{len(openpyxl.load_workbook('./out.xlsx').sheetnames)+1}"):
     a = []
     for i in range(len(axisX)):
         a.append(' ')
-    with pandas.ExcelWriter(path='./out.xlsx', mode='a', engine='openpyxl', if_sheet_exists="new") as writer:
-        pandas.DataFrame({"x":axisX, "y":axisY, " ": a}).to_excel(excel_writer=writer, sheet_name=f"Sheet {name}")
+    dataToAppend = pandas.DataFrame([axisX, axisY, a], columns=['x', 'y', ' '])
+    with pandas.ExcelFile(path_or_buffer='./out.xlsx') as reader:
+        data = reader.parse(sheet_name=name)
+    with pandas.ExcelWriter(path='./out.xlsx', mode='a', engine='openpyxl', if_sheet_exists="overlay") as writer:
+        pandas.DataFrame(pandas.concat([data, dataToAppend], axis=1)).to_excel(excel_writer=writer, sheet_name=name)
+        writer.close()
         
--- main.py
+++ main.py
@@ -8,25 +8,25 @@ metric = []
 accuracy = []
 print(f"Using {computeDevice} as compute accelerator...")
 print("Variation: Batch Size")
-# This will loop until the change in accuracy between variations is less than 0.5%, i.e. we've reached the point of diminishing returns
-for i in range(1, 50000):
+# This will loop until the change in accuracy between variations is less than 0.1%, i.e. we've reached the point of diminishing returns
+for i in range(16):
     # Create an instance of the CIFAR-10 Neural Network model
     # Params: batchSize, device, iterations, learningRate
     acc = 0
-    batchSize=i
-    metric.append(batchSize)
-    model = cifar10_nn.CIFAR10_NN(batchSize, computeDevice, 5, 1e-2).to(computeDevice)
+    varying = 2**i
+    metric.append(varying)
+    model = cifar10_nn.CIFAR10_NN(varying, computeDevice, 5, 1e-2).to(computeDevice)
     # Run one training iteration
     # - Use the model instance we created alongside the CIFAR-10 dataset
     # - Our loss function will be Cross Entropy Loss, which is very useful for categorization neural networks
     # - Our optimizer will be Stochastic Gradient Descent with a learning rate of 1%. 
     #   - All this does is makes iteration more efficient. Its inner-workings are far too complex to explain in comments.
-    print(f"\nVarying parameter value: {i}")
+    print(f"\nVarying parameter value: {varying}")
     for j in range(model.iterations):
         trainer.iterate(model.trainingDataLoader, model, torch.nn.CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr=model.learningRate))
         acc = trainer.test(model.testDataLoader, model, torch.nn.CrossEntropyLoss())
     accuracy.append(acc)
-    if ((accuracy[0 if i == 1 else i-2] - accuracy[i-1]) <= 0.005) and i > 14: break
+    if (abs(accuracy[0 if i == 0 else i-1] - accuracy[i]) <= 0.1): break
 # Export the data to an existing sheet
 analysis.exportResults(metric, accuracy)
     
